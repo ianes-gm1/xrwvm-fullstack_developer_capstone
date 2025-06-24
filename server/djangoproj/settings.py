fb28fd37d1672c5c7fcbd0701f3a73123a9d5578
Ianis <90624583+ianes-gm1@users.noreply.github.com>
2025-06-24 18:32:58 +0200

    module 5

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,14 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://ianespreala-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['localhost', ('https://ianespreala-8000.theiadockernext-1-'
+                               'labs-prod-theiak8s-4-tor01.proxy.'
+                               'cognitiveclass.ai/')]
 CSRF_TRUSTED_ORIGINS = [
-    'https://https://ianespreala-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
-    'https://ianespreala-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ('https://https://ianespreala-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+     'tor01.proxy.cognitiveclass.ai/'),
+    ('https://ianespreala-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+     'proxy.cognitiveclass.ai')
 ]
 
 REST_FRAMEWORK = {
@@ -65,8 +69,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
-            os.path.join(BASE_DIR,'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
@@ -96,8 +100,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator')
     },
     {
         'NAME':
@@ -142,8 +146,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
